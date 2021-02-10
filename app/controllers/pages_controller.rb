371f3d28a8a3dd1ee58e15797e34392c09468ef0
albane tonnellier <albanetonnellier@gmail.com>
2021-02-10 18:32:24 +0000

    routes added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+	def about
+	end 
+
+	def home
+	end
+
+	def blog
+	end
+	
+	def professional 
+	end 
+	
+	def conditions
+	end 
+	
+	def privacy 
+	end 
+	
+	def cookies
+	end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'blog', to: 'pages#blog'
+  get 'professional', to: 'pages#professional'
+  get 'conditions' , to: 'pages#conditions'
+  get 'privacy' , to: 'pages#privacy'
+  get 'cookies', to: 'pages#cookies'
 end
