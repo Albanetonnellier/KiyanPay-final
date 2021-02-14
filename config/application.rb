76a224f51342b3755ca92e46e63585ad2283dd1e
albane tonnellier <albanetonnellier@gmail.com>
2021-02-14 19:19:48 +0000

    rt

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.hosts << /.*/
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,7 +11,7 @@ module KiyanPayfinal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.hosts << /.*/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
